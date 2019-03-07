dc7b4ab750b762e906cce8b190a06a7437d08d49
Jan Fluksa <janfluksa@outlook.com>
2019-03-07 13:14:26 +0100

    init

---
+++ main.js
@@ -0,0 +1,21 @@
+var {Rectangle, Color} = require("scenegraph");
+
+function removeBorder (selection) {
+    
+    let s = selection;
+
+    if (s.items.length < 1) {
+        console.log("Remove border plugin: Select the object first.")
+    }
+
+    for (let i=0;i<s.items.length;i++) {
+        s.items[i].strokeWidth = 0;
+        s.items[i].strokeEnabled = false;
+    }
+}
+
+module.exports = {
+    commands: {
+        removeBorder: removeBorder
+    }
+};
